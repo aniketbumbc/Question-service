346e741c4cfcf290fcf30046a24e2e7169db74cc
aniket <aniket.umbc@gmail.com>
2023-08-27 13:37:34 +0530

    Initial service setup

---
+++ src/main/java/com/bunny/questionservice/model/QuestionWrapper.java
@@ -0,0 +1,67 @@
+package com.bunny.questionservice.model;
+
+public class QuestionWrapper {
+	
+	private Integer id;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public QuestionWrapper(Integer id, String option1, String option2, String option3, String option4, String level,
+			String questionTitle) {
+		super();
+		this.id = id;
+		this.option1 = option1;
+		this.option2 = option2;
+		this.option3 = option3;
+		this.option4 = option4;
+		this.level = level;
+		this.questionTitle = questionTitle;
+	}
+	public String getOption1() {
+		return option1;
+	}
+	public void setOption1(String option1) {
+		this.option1 = option1;
+	}
+	public String getOption2() {
+		return option2;
+	}
+	public void setOption2(String option2) {
+		this.option2 = option2;
+	}
+	public String getOption3() {
+		return option3;
+	}
+	public void setOption3(String option3) {
+		this.option3 = option3;
+	}
+	public String getOption4() {
+		return option4;
+	}
+	public void setOption4(String option4) {
+		this.option4 = option4;
+	}
+	public String getLevel() {
+		return level;
+	}
+	public void setLevel(String level) {
+		this.level = level;
+	}
+	public String getQuestionTitle() {
+		return questionTitle;
+	}
+	public void setQuestionTitle(String questionTitle) {
+		this.questionTitle = questionTitle;
+	}
+	private String option1;
+	private String option2;
+	private String option3;
+	private String option4;
+	private String level;
+	private String questionTitle;
+
+
+}
---
+++ src/main/java/com/bunny/questionservice/service/QuestionService.java
@@ -0,0 +1,61 @@
+package com.bunny.questionservice.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.bunny.questionservice.doa.QuestionDao;
+import com.bunny.questionservice.model.Question;
+
+
+
+@Service
+public class QuestionService {
+
+	@Autowired
+	QuestionDao questionDoa;
+
+	public ResponseEntity<List<Question>> getAllQuestions() {
+
+		try {
+			return new ResponseEntity<>(questionDoa.findAll(), HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+
+	}
+
+	public ResponseEntity<List<Question>> getQuestionByCategory(String category) {
+
+		try {
+			return new ResponseEntity<>(questionDoa.findByCategory(category), HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+
+	}
+
+	public ResponseEntity<String> addQuestion(Question question) {
+
+		try {
+			questionDoa.save(question);
+			return new ResponseEntity<>("Data Successfully Save", HttpStatus.CREATED);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return new ResponseEntity<>("Something went wrong", HttpStatus.BAD_REQUEST);
+
+	}
+
+	public String deletQuestionById(Integer id) {
+		questionDoa.deleteById(id);
+		return "Data Successfully Delete " + id;
+	}
+
+}
